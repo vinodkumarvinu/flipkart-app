5258b4dea39bc7310787f2861fa33122e26b8549
vinodkumarvinu <pavanvenky111@gmail.com>
2022-08-29 21:00:39 +0530

    created addition.java file

---
+++ addition.java
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+class Addition
+{
+        public static void main(String args[])
+        {
+                int num1,num2,res;
+                Scanner s =new Scanner(System.in);
+                System.out.println("Enter num1 value");
+                num1=s.nextInt();
+                System.out.println("Enter num2 value");
+                num2=s.nextInt();
+                res=num1+num2;
+                System.out.println("The sum of given numbers is "+res);
+        }
+}
+
+
