dcd2333633af6b1a190b19026917d6ad18b2113f
vinodkumarvinu <pavanvenky111@gmail.com>
2022-08-31 18:56:22 +0530

    changes occur

--- addition.java
+++ addition.java
@@ -17,7 +17,7 @@ class Addition
                 num1=s.nextInt();
                 System.out.println("Enter num2 value");
                 num2=s.nextInt();
-                res=num1+num2;
+                res=num1/num2;
                 System.out.println("The sum of given numbers is "+res);
                 //This is End of the File
 	       System.out.println("n2 value is :"+n2);
