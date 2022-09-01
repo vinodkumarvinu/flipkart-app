d7eb085d481fe55ec085f21b45d32d3441dc38a0
vinodkumarvinu <112389480+vinodkumarvinu@users.noreply.github.com>
2022-09-01 09:38:36 +0530

    added changes

--- addition.java
+++ addition.java
@@ -11,9 +11,7 @@ class Addition
                 int num1,num2,res;
                 Scanner s =new Scanner(System.in);
 		n1=25;
-		     //Tis is first comment
                 Scanner s =new Scanner(System.in);
-                //Scanner class contains nextInt() which reads the integer value
                 Scanner s =new Scanner(System.in);
                 System.out.println("Enter num1 value");
                 System.out.println("Enter num1 value");
@@ -24,6 +22,7 @@ class Addition
                 System.out.println("The sum of given numbers is "+res);
                 //This is End of the File
 	       System.out.println("n2 value is :"+n2);
+		//Scanner class contains nextInt() which reads the integer value
 	
        }
 }
