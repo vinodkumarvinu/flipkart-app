2fdb8eda96878f8d3233d97e11ffe5962c53237b
vinodkumarvinu <pavanvenky111@gmail.com>
2022-09-01 09:48:57 +0530

    extra code added

--- addition.java
+++ addition.java
@@ -8,7 +8,7 @@ class Addition
 {
         public static void main(String args[])
         {
-                int num1,num2,res;
+                int num1,num2,n1,n2,res;
                 Scanner s =new Scanner(System.in);
 		n1=25;
                 Scanner s =new Scanner(System.in);
@@ -19,9 +19,11 @@ class Addition
                 System.out.println("Enter num2 value");
                 num2=s.nextInt();
                 res=num1/num2;
+		n2=n1++;
                 System.out.println("The sum of given numbers is "+res);
                 //This is End of the File
-	       System.out.println("n2 value is :"+n2);
+		//System.out.println("n2 value is :"+n2);
+
 		//Scanner class contains nextInt() which reads the integer value
 	
        }
